37243fb334caef6f5cb82ca1370508d2dc4ad82f
Hu, Smile <smile.hu@sap.com>
2017-10-03 18:10:41 +0800

    添加作为网页和后台的接口使用demo.

--- app/main.py
+++ app/main.py
@@ -9,13 +9,28 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 import os
+import json
 from flask import Flask, render_template
 app = Flask(__name__)
 
+'''
+WebsiteURL: 作为网站的接口
+'''
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", data)
 
+'''
+BackendApi: 作为后台的接口
+'''
+@app.route('/backend/api/test1')
+def api_test1():
+	data = {"testk1": "testv1"}
+	return json.dumps(data).decode('unicode-escape')
+
+'''
+Main Entry: 主入口
+'''
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     # host='0.0.0.0', 这会让操作系统监听所有公网 IP,服务器和局域网才能通过你本机ip访问
